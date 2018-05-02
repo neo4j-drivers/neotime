ac5471f5c497bf3743806ba8bcb58ae14b86bd74
Nigel Small <nigel@neo4j.com>
2018-05-02 15:45:39 +0100

    Version RC1

--- neotime/__init__.py
+++ neotime/__init__.py
@@ -33,7 +33,7 @@ from neotime.arithmetic import (nano_add, nano_sub, nano_mul, nano_div, nano_mod
 from neotime.metaclasses import DateType, TimeType, DateTimeType
 
 
-__version__ = "1.0.0b6"
+__version__ = "1.0.0rc1"
 
 
 MIN_INT64 = -(2 ** 63)
